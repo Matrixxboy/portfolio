bc666cc3bff05286c67c568fc248784807da7934
Utsav Lankapati <matrix.utsav.lankapati@gmail.com>
2025-05-01 13:01:44 +0530

    footer update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','The Matrix World')</title>
     @vite (['resources/js/app.js'])
+    @vite (['resources/js/particleBackground.js'])
+
 
 </head>
 <body class="relative bg-color-primary overflow-x-hidden">
     <!-- Background SVG Layer -->
-    <div class="fixed bottom-0 left-0 z-0 w-full h-full  opacity-1 pointer-events-none">
+    <div class="fixed bottom-0 left-0 z-0 w-full h-full  opacity-2 pointer-events-none">
         <img src="{{ asset('media/matrix-bg.svg') }}"
             alt="Background SVG"
             class="w-full h-full object-contain filter invert" />
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,12 @@
-<div class="container mx-auto grid gap-8 grid-row-2 text-center glass-white text-color-secondary">
+<div class="mx-auto grid grid-rows-8 text-center glass-white text-color-secondary border-1">
         <!-- footer main content -->
-        <div class="grid grid-cols-5 row-span-1 py-4">
-            <!-- Main : column 1 title and navigation -->
-            <div class="col-span-1">
-                <h2 class="text-2xl font-bold">The Matrix World</h2>
-                <p class="text-sm text-gray-300 mt-1">Grow · Learn · Succeed</p>
-                <ul class="mt-4 space-y-2 text-sm">
+        <div class="grid grid-cols-7 row-span-6 py-4 ">
+
+        <!-- Main : column 1 title and navigation -->
+            <div class="col-span-2 ">
+                <h2 class="text-4xl font-bold">The Matrix World</h2>
+                <p class="text-xl text-gray-300 mt-1">Grow · Learn · Succeed</p>
+                <ul class="mt-4 space-y-2 text-lg">
                     <li><a href="#" class="hover:underline">Home</a></li>
                     <li><a href="#" class="hover:underline">About Me</a></li>
                     <li><a href="#" class="hover:underline">Projects</a></li>
@@ -13,21 +14,71 @@
                     <li><a href="#" class="hover:underline">Blogs</a></li>
                 </ul>
             </div>
+
             <!-- main : column 4 social media and cotacts -->
-            <div class="col-span-4 grid grid-rows-2">
-                <!-- section one social media -->
-                <div class="row-span-1 grid-cols-3 grid-rows-3">
+            <div class="col-span-5 grid grid-rows-2 ">
 
+                <!--row first social media  -->
+                <div class="row-span-1 grid grid-cols-5 text-left">
+                    <!-- section one professional account social media -->
+                    <div class="grid grid-rows-3 col-span-1 ">
+                    <div class="row-span-1 grid grid-cols-6 gap-1">
+                        <div class="col-span-2">
+                            <a href="https://github.com/your-username" target="_blank">
+                                <svg class="w-full h-full fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path d="" />
+                                </svg>
+                            </a>
+                        </div>
+
+                    </div>
+                        <div class="row-span-1">
+                            <h3>Social 2</h3>
+                        </div>
+                        <div class="row-span-1">
+                            <h3>Social 3</h3>
+                        </div>
+                    </div>
+                    <!-- section two basic social media -->
+                    <div class="grid grid-rows-3 col-span-1">
+                        <div class="row-span-1">
+                            <h3>Social 1</h3>
+                        </div>
+                        <div class="row-span-1">
+                            <h3>Social 2</h3>
+                        </div>
+                        <div class="row-span-1">
+                            <h3>Social 3</h3>
+                        </div>
+                    </div>
+                    <!-- section three contect info -->
+                    <div class="grid grid-rows-3 col-span-3 ">
+                        <div class="row-span-1">
+                            <h3>Social 1</h3>
+                        </div>
+                        <div class="row-span-1">
+                            <h3>Social 2</h3>
+                        </div>
+                    </div>
                 </div>
-                <!-- section second cotacts -->
-                <div class="row-2">
-
+                <!-- row second constact section -->
+                <div class="row-span-1 grid grid-cols-2">
+                    <div class="col-span-1 grid grid-rows-5">
+                        <div class="row-span-1 "></div>
+                        <div>
+                            <a href="#contact"
+                                class="mx-auto px-6 py-2 text-lg font-semibold text-purple-100 bg-white/10 rounded-md backdrop-blur-md transition duration-300 hover:bg-purple-400/10 hover:shadow-[0_0_12px_2px_rgba(168,85,247,0.4)] animate-pulse hover:animate-none">
+                                Contact Now
+                            </a>
+                        </div>
+                        <div class="row-span-1 "></div>
+                    </div>
                 </div>
             </div>
         </div>
 
         <!-- footer copyright section -->
-        <div>
+        <div class="row-span-2">
             <hr class="my-6 border-gray-600" />
             <div class="text-xs text-center text-gray-400 space-x-2">
                 <a href="#" class="hover:underline">Website Terms & Conditions</a> |
