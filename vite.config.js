6c50d5db9187b9b8a13441e8ba728669d09498d5
Utsav Lankapati <matrix.utsav.lankapati@gmail.com>
2025-05-03 14:54:45 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  base: './', // 👈 important for Vercel
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
 });
